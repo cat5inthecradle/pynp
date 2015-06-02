700f4d7bbbd6b697c740fae8c45c31b71339ec2f
Darin Webb <dwebb@mis-solutions.com>
2015-06-01 23:50:21 -0500

    Added Check methods and abstracted API calls

--- pynp.py
+++ pynp.py
@@ -1,28 +1,35 @@
-## pynp.py
+# pynp.py
+# author: Darin Webb @Cat5InTheCradle
 
-# http://docs.python-requests.org/en/latest/
 import requests
 
 class NodePingInterface:
 
 	accounts = {}
+	checks = {}
 
-	def __init__(self, apitoken):
+	def __init__(self, apitoken, update = False):
 		self.apitoken = apitoken
+		if update:
+			update_all()
+
+	def update_all(self):
 		self.update_accounts()
+		self.update_checks()
+
+	# Accounts
+	# -------------------------
 
 	def update_accounts(self):
-		payload = {'token':self.apitoken}
-		response = requests.get('https://api.nodeping.com/api/1/accounts', json=payload).json()
+		response = self.get_accounts()
 		for customerid in response:
 			self.accounts[customerid] = NPAccount(self.get_account(customerid))
 
+	def get_accounts(self):
+		return self.api_accounts_get()
+
 	def get_account(self, customerid):
-		payload = {
-			'token':self.apitoken,
-			'customerid':customerid
-		}
-		response = requests.get('https://api.nodeping.com/api/1/accounts', json=payload).json()
+		response = self.api_accounts_get({'customerid':customerid})
 		return response
 
 	def new_account(self, name, contactname, email, timezone="-5", location="nam", emailme="no"):
@@ -39,13 +46,60 @@ class NodePingInterface:
 		self.accounts[response['_id']] = NPAccount(self.get_account(response['_id']))
 		return response
 
+	# Checks
+	# --------------------------
 
-class NPAccount(object):
+	def update_checks(self):
+		parent_checks = self.get_checks()
+		for check_id in parent_checks:
+			self.checks[check_id] = NPCheck(parent_checks[check_id])
+		for account in self.accounts:
+			account_checks = self.get_checks(self.accounts[account].details['_id'])
+			for check_id in account_checks:
+				self.checks[check_id] = NPCheck(account_checks[check_id])
 
+	def get_checks(self, customerid = "", lastresult = False):
+		payload = {'lastresult':False}
+		if subaccount != "":
+			payload['customerid'] = customerid
+		return self.api_checks_get(payload)
+
+	def get_check(self, check_id, current = False):
+		customerid = check_id.split('-')[0]
+		return self.api_checks_get({'customerid':customerid, 'id':check_id, 'current':False})
+
+	# API Requests
+	# ---------------------------
+
+	def api_accounts_get(self, payload = {}):
+		payload['token'] =  self.apitoken
+		return requests.get('https://api.nodeping.com/api/1/accounts', json=payload).json()
+
+	def api_checks_get(self, payload = {}):
+		payload['token'] = self.apitoken
+		return requests.get('https://api.nodeping.com/api/1/checks', json=payload).json()
+
+class NPObject(object):
 	details = {}
-	
 	def __init__(self, details):
 		self.details = details
 
-	def get_details(self):
-		return {'name':self.details['customer_name'], '_id':self.details['_id']}
+class NPAccount(NPObject):
+	checks = {}
+	children = {}
+
+	# feed this npi.get_checks(self.details['_id'])
+	def load_checks(self, checks):
+		for check_id in checks:
+			self.checks[check_id] = NPCheck(checks[check_id])
+
+	def load_children(self, subaccounts):
+		for customerid in subaccounts:
+			self.children[subaccount] = NPAccount(subaccounts[customerid])
+
+	def pprint(self):
+		return "Account ID: " + self.details['_id'] + ", " + self.details['customer_name']
+
+class NPCheck(NPObject):
+	def pprint(self):
+		return "Check ID: " + self.details['_id'] + ", " + self.details['label']
