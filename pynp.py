359a910c37b863686684819cfb3e66a8b5f9fd43
Darin Webb <darinlwebb@gmail.com>
2015-09-21 22:00:51 -0500

    added NPCheck

--- pynp.py
+++ pynp.py
@@ -5,10 +5,14 @@ import requests
 
 class NodePingInterface:
 
-	def __init__(self, apitoken, update = False):
+	def __init__(self, apitoken, update_accounts = False, update_checks = False):
 		self.apitoken = apitoken
-		if update:
+
+		# update_checks implies update_accounts for now
+		if update_checks:
 			self.update_all()
+		elif update_accounts:
+			self.update_accounts()
 
 	def update_all(self):
 		self.update_accounts()
@@ -17,6 +21,7 @@ class NodePingInterface:
 	# Accounts
 	# -------------------------
 
+	# Sets the parent_account property and populates subaccounts
 	def update_accounts(self):
 		self.parent_account = None
 
@@ -27,7 +32,7 @@ class NodePingInterface:
 				self.parent_account = NPAccount(self.get_account(customerid))
 			else:
 				subaccounts[customerid] = self.get_account(customerid)
-		self.parent_account.set_children(subaccounts)
+		self.parent_account.set_subaccounts(subaccounts)
 
 	def get_accounts(self):
 		return self.api_accounts_get()
@@ -53,25 +58,26 @@ class NodePingInterface:
 	# --------------------------
 
 	def update_checks(self):
-		self.checks = {}
 
-		parent_checks = self.get_checks()
-		for check_id in parent_checks:
-			self.checks[check_id] = NPCheck(parent_checks[check_id])
-		for account in self.parent_account.children:
-			account_checks = self.get_checks(self.parent_account.children[account].details['_id'])
-			for check_id in account_checks:
-				self.checks[check_id] = NPCheck(account_checks[check_id])
+		self.parent_account.set_checks(self.get_checks())
 
-	def get_checks(self, customerid = "", lastresult = False):
-		payload = {'lastresult':False}
+		for customer_id in self.parent_account.subaccounts:
+			subaccount = self.parent_account.subaccounts[customer_id]
+			subaccount.set_checks(self.get_checks(customer_id, True))
+
+	# if no customerid given, returns parent account checks only.
+	def get_checks(self, customerid = "", current = False):
+		payload = {'current':current}
 		if customerid != "":
 			payload['customerid'] = customerid
 		return self.api_checks_get(payload)
 
-	def get_check(self, check_id, current = False):
+	def get_check(self, check_id, lastresult = False):
 		customerid = check_id.split('-')[0]
-		return self.api_checks_get({'customerid':customerid, 'id':check_id, 'current':False})
+		return self.api_checks_get({'customerid':customerid, 'id':check_id, 'lastresult':lastresult})
+
+	def check_status(self, check_id):
+		return self.get_check(check_id, True)['lastresult']['su']
 
 	# API Requests
 	# ---------------------------
@@ -84,27 +90,51 @@ class NodePingInterface:
 		payload['token'] = self.apitoken
 		return requests.get('https://api.nodeping.com/api/1/checks', json=payload).json()
 
+	def api_results_current_get(self, payload = {}):
+		payload['token'] = self.apitoken
+		return requests.get('https://api.nodeping.com/api/1/results/current', json=payload).json()
+
 class NPObject(object):
-	details = {}
 	def __init__(self, details):
 		self.details = details
 
+	def get(self, key):
+		try:
+			return self.details(key)
+		except E as N:
+			return None
+
+	def print_details(self):
+		output = ""
+		for key in sorted(details):
+			output += key + ": " + details(key) + "\n"
+		return output
+
+
 class NPAccount(NPObject):
-	checks = {}
-	children = {}
+
+	def __init__(self, details):
+		self.checks = {}
+		self.subaccounts = {}
+		super(NPAccount, self).__init__(details)
 
 	# feed this npi.get_checks(self.details['_id'])
 	def set_checks(self, checks):
 		for check_id in checks:
 			self.checks[check_id] = NPCheck(checks[check_id])
 
-	def set_children(self, subaccounts):
+
+	def set_subaccounts(self, subaccounts):
 		for customerid in subaccounts:
-			self.children[customerid] = NPAccount(subaccounts[customerid])
+			self.subaccounts[customerid] = NPAccount(subaccounts[customerid])
 
 	def pprint(self):
 		return self.details['_id'] + ", " + self.details['customer_name']
 
 class NPCheck(NPObject):
+
+	def __init__(self, details):
+		super(NPCheck, self).__init__(details)
+
 	def pprint(self):
 		return self.details['_id'] + ", " + self.details['label']
